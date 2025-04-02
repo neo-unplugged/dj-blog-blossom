bf27a285cfe9ce92e69df6311d1a5a5de45d3dbb
Atik Ahmed Mishu <atik.wowspace@gmail.com>
2025-04-02 11:54:12 +0600

    removed redundant files

--- blossom/settings.py
+++ blossom/settings.py
@@ -132,7 +132,6 @@ STATICFILES_DIRS = [
     BASE_DIR/ 'static'
 ]
 
-STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
